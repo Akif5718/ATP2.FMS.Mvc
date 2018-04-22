5b731126e8f66b5b0b4262f14302ca9f06ca793b
Akif5718 <35718815+Akif5718@users.noreply.github.com>
2018-04-23 03:24:23 +0600

    Authorize with some update

--- V1/ATP2.FMS.Mvc/Controllers/UserController.cs
+++ V1/ATP2.FMS.Mvc/Controllers/UserController.cs
@@ -12,7 +12,7 @@ using Newtonsoft.Json;
 
 namespace ATP2.FMS.Mvc.Controllers
 {
-    [Authorize]
+
     public class UserController : BaseController
     {
         // GET: User
@@ -81,7 +81,8 @@ namespace ATP2.FMS.Mvc.Controllers
 
 
         }
-        [AllowAnonymous]
+
+        [HttpGet]
         public ActionResult LoginForm()
         {
 
